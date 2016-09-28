1a7e917909cc963f57f2218a96484283e1c596b8
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2016-09-29 01:36:37 +0900

    Drawerが動くようになった

--- app/src/main/java/com/mapyo/findtravel/MainActivity.java
+++ app/src/main/java/com/mapyo/findtravel/MainActivity.java
@@ -1,13 +1,43 @@
 package com.mapyo.findtravel;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.mapyo.findtravel.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setActivity(this);
+
+        setSupportActionBar(binding.toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, binding.drawerLayout, binding.toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        binding.drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        binding.navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        binding.drawerLayout.closeDrawer(GravityCompat.START);
+
+        // todo 選択されたメニューによってどうするか決める
+        return true;
     }
 }
