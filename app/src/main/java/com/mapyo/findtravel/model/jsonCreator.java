c1a45c4fa1dfb9375ceef5d818ad067d84fe031d
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2016-09-30 21:12:39 +0900

    Assetsのjsonファイルを読み込んで文字列として返すやつを作った

---
+++ app/src/main/java/com/mapyo/findtravel/model/jsonCreator.java
@@ -0,0 +1,18 @@
+package com.mapyo.findtravel.model;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class jsonCreator {
+    public static String readFromAssets(Context context, String fileName) throws IOException {
+        AssetManager assetManager = context.getResources().getAssets();
+        InputStream inputStream = assetManager.open(fileName);
+
+        byte[] readBytes = new byte[inputStream.available()];
+        inputStream.read(readBytes);
+        return new String(readBytes);
+    }
+}
