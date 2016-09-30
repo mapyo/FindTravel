cf1f528866adc8985cde434926a7cfdb2957406a
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2016-09-30 09:22:25 +0900

    一通りjsonを作るスクリプトを書いた

--- scraping/ranking.js
+++ scraping/ranking.js
@@ -3,12 +3,23 @@ var client = require('cheerio-httpcli');
 var baseUrl = 'http://find-travel.jp/ranking/yesterday'
 
 client.fetch(baseUrl, {  }, function (err, $, res) {
-    $(".article").each(function (idx) {
-      var image = $(this).find('.article-image-sq').attr('src');
-      var link = baseUrl + $(this).find('a').attr('href');
-      var title = $(this).find('.article-title').text().trim();
-      var views = $(this).find('.view-count').text();
 
-      process.exit();
-    });
+  var articleList = new Array();
+
+  $(".article").each(function (idx) {
+    var image = $(this).find('.article-image-sq').attr('src');
+    var link = baseUrl + $(this).find('a').attr('href');
+    var title = $(this).find('.article-title').text().trim();
+    var views = $(this).find('.view-count').text();
+
+    var article = {};
+    article.image = image;
+    article.link = link;
+    article.title = title;
+    article.views = views;
+
+    articleList.push(article);
+  });
+
+  console.log(articleList);
 });
