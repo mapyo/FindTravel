ea55f6312babf042dd4b51d8a90a23a6f18adfd2
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2016-10-02 11:10:51 +0900

    topicをスクレイピングするjsonを作った

--- scraping/topic.js
+++ scraping/topic.js
@@ -2,6 +2,13 @@ var client = require('cheerio-httpcli');
 
 var baseUrl = 'http://find-travel.jp/topic'
 
+function getImageUrl(backgroundImageUrl) {
+  return backgroundImageUrl.slice(
+    backgroundImageUrl.indexOf("'") + 1,
+    backgroundImageUrl.lastIndexOf("'"));
+}
+
+
 client.fetch(baseUrl, {  }, function (err, $, res) {
 
   var pickupList = new Array();
@@ -15,9 +22,7 @@ client.fetch(baseUrl, {  }, function (err, $, res) {
 
     var backgroundImageUrl = $(this).find('div').css('background-image');
 
-    var image = backgroundImageUrl.slice(
-      backgroundImageUrl.indexOf("'") + 1,
-      backgroundImageUrl.lastIndexOf("'"));
+    var image = getImageUrl(backgroundImageUrl);
 
     var pickup = {};
     pickup.id = id;
@@ -27,8 +32,27 @@ client.fetch(baseUrl, {  }, function (err, $, res) {
     pickupList.push(pickup);
   });
 
+  $(".topic-grid__item").each(function (idx) {
+    var linkPath = $(this).find('a').attr('href');
+    var backgroundImageUrl = $(this).find('.topic-grid__item__image').css('background-image');
+
+    var id = linkPath.slice(linkPath.lastIndexOf('/') + 1);
+    var link = baseUrl + linkPath;
+    var image = getImageUrl(backgroundImageUrl);
+    var title = $(this).find('.topic-grid__item__name').find('a').text();
+
+    var feature = {};
+    feature.id = id;
+    feature.link = link;
+    feature.image = image;
+    feature.title = title;
+
+    featureList.push(feature);
+  });
+
   var jsonObject = {};
-  jsonObject.data = pickupList;
+  jsonObject.pickup_features = pickupList;
+  jsonObject.features = featureList;
 
   console.log(JSON.stringify(jsonObject));
 });
